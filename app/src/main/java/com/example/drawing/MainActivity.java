b0044013e34abf5034f46c52f355575d7eea0407
martintung <martin25tung@gmail.com>
2021-07-01 15:56:17 +0800

    PropertyValuesHolder.ofKeyframe

--- app/src/main/java/com/example/drawing/MainActivity.java
+++ app/src/main/java/com/example/drawing/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.drawing;
 
+import android.animation.Keyframe;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.drawing.view.CameraView;
 
 public class MainActivity extends AppCompatActivity {
 
-  CameraView view;
+  View view;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +44,25 @@ public class MainActivity extends AppCompatActivity {
     animatorSet.setStartDelay(1000);
     animatorSet.start();*/
 
-    PropertyValuesHolder bottomFlipHolder = PropertyValuesHolder.ofFloat("bottomFlip", 45);
+    /*PropertyValuesHolder bottomFlipHolder = PropertyValuesHolder.ofFloat("bottomFlip", 45);
     PropertyValuesHolder flipRotationHolder = PropertyValuesHolder.ofFloat("flipRotation", 270);
     PropertyValuesHolder topFlipHolder = PropertyValuesHolder.ofFloat("topFlip", -45);
     ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(view, bottomFlipHolder, flipRotationHolder, topFlipHolder);
     objectAnimator.setStartDelay(1000);
     objectAnimator.setDuration(2000);
-    objectAnimator.start();
+    objectAnimator.start();*/
 
+    // 客製化中間的流程  時間曲線
+    float length = Utils.dp2px(300);
+    Keyframe keyframe1 = Keyframe.ofFloat(0, 0);
+    Keyframe keyframe2 = Keyframe.ofFloat(0.2f, 1.5f * length);
+    Keyframe keyframe3 = Keyframe.ofFloat(0.8f, 0.6f * length);
+    Keyframe keyframe4 = Keyframe.ofFloat(1f, 1f * length);
+
+    PropertyValuesHolder viewHolder = PropertyValuesHolder.ofKeyframe("translationX", keyframe1, keyframe2, keyframe3, keyframe4);
+    ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(view, viewHolder);
+    animator.setStartDelay(1000);
+    animator.setDuration(2000);
+    animator.start();
   }
 }
